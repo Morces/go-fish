cfa0ebd1482adf7f8600b0f2b58a6153f058f2c3
lydiah1wachira <lydiahcedric@gmail.com>
2022-05-18 23:46:24 +0300

    installed required dependencies.

---
+++ app/__init__.py
@@ -0,0 +1,5 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/moringa/Documents/PROJECTS/gofish/virtual"
+set -gx VIRTUAL_ENV "/home/wachira/Documents/go-fish/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
