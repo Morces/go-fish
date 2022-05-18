336e264ebf4faaa0cb647623a6968778994b2389
lydiah1wachira <lydiahcedric@gmail.com>
2022-05-19 00:53:27 +0300

    add an email column to the users model and migrate to updated db.

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,8 @@ bootstrap = Bootstrap()
 mail = Mail()
 db = SQLAlchemy()
 bcrypt =Bcrypt()
+
+
 login_manager=LoginManager()
 login_manager.login_view ='login'
 login_manager.login_message_category='info'
