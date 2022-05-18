27f730305bc6626745d4c8b836991f1795d3bffc
lydiah1wachira <lydiahcedric@gmail.com>
2022-05-19 01:16:23 +0300

    add a login form.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -26,4 +26,12 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         
         if user:
-            raise ValidationError('That email is taken.Please try a different one')        
+            raise ValidationError('That email is taken.Please try a different one')   
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')     
