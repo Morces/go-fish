8461e22fd5b690a2b1a6dfa509e317f630f6d0f1
mercyakuku <mercyakuku24@gmail.com>
2022-05-19 09:23:53 +0300

    Created and updated files

---
+++ config.py
@@ -0,0 +1,14 @@
+class Config:
+    SECRET_KEY ="mercy"
+
+class ProdConfig(Config):
+    
+    pass
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
